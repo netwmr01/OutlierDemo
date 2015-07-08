a2e26331fff452543becb3ca96106e20b428f2f2
Tommzy <hzheng@wpi.edu>
2015-07-08 10:48:00 -0400

    Finish restful service setup

--- src/main/java/controllers/MethodController.java
+++ src/main/java/controllers/MethodController.java
@@ -2,16 +2,21 @@ package controllers;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 import java.util.Map;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import demo.dsrg.cs.wpi.edu.*;
 import util.dsrg.cs.wpi.edu.SortedCandidate;
 
 @RestController
 public class MethodController {
+	UserStudy userStudy;
+	
+	public MethodController(){
+		userStudy = new UserStudy();
+	}
 
 	@RequestMapping("/methods")
 	public Data greeting(@RequestParam(value="number", defaultValue="1") String number) {
@@ -19,14 +24,18 @@ public class MethodController {
 	}
 
 	@RequestMapping("/method1")
-	public String getMethod1(@RequestParam Map<String,String> requestParams){
-		int k=Integer.valueOf(requestParams.get("k"));
+	public @ResponseBody Collection<SortedCandidate> getMethod1(@RequestParam(value="k") String k, @RequestParam(value="r") String r){
+		int intk=Integer.valueOf(k);
 		System.out.println(k);
-		double r=Double.valueOf(requestParams.get("r"));
+		double doubler=Double.valueOf(r);
 		System.out.println(r);
-		UserStudy userStudy = new UserStudy();
-		Collection<SortedCandidate> result = userStudy.getMethod1(k, r);
-		return result.toArray().toString();
+		Collection<SortedCandidate> result = userStudy.getMethod1(intk, doubler);
+		return result;
+	}
+	
+	@RequestMapping("/getInitData")
+	public @ResponseBody ArrayList<ArrayList<SortedCandidate>> getAllPoints(){
+		return userStudy.getDataPlane();
 	}
 
 }
