54f6d2fba60d3ecdc8871bc9cb308bafb43cb220
Tommzy <hzheng@wpi.edu>
2015-07-11 15:34:07 -0400

    Simple update, nothing change

--- src/main/java/controllers/WebConfig.java
+++ src/main/java/controllers/WebConfig.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import java.util.GregorianCalendar;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -15,7 +16,7 @@ public class WebConfig extends WebMvcConfigurerAdapter{
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 	   registry.addResourceHandler("/resources/**").addResourceLocations("/resources/").setCachePeriod(31556926);
-	   System.out.println("ResourcesHandeler Registry!");
+	   System.out.println(GregorianCalendar.getInstance().getTime() + "ResourcesHandeler Registry Finished.");
 	}
 
 }
